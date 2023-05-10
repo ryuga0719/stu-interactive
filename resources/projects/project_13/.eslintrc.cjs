f11d85c10b3a99d9f2c04fe50a648af38112e81d
ryuga0719 <ryuga.private@gmail.com>
2023-05-10 11:57:51 +0900

    add .vscode

--- resources/projects/project_13/.eslintrc.cjs
+++ resources/projects/project_13/.eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
+    'prettier',
   ],
   overrides: [],
   parser: '@typescript-eslint/parser',
